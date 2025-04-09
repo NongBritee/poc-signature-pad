5db6e630e7c75eed55b37812af81155b8404160e
NongBritee <berkito_99@hotmail.com>
2025-04-09 10:14:30 +0700

    feat: sig pad

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import SignatureCanvas from 'react-signature-canvas'
+import { useEffect, useRef, useState } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,102 +14,48 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+  const sigCanvas = useRef(null);
+  const [url, setUrl] = useState(null);
+
+  const onClear = () => {
+    sigCanvas.current.clear();
+  };
+  const onSave = () => {
+    setUrl(sigCanvas.current.getTrimmedCanvas().toDataURL('image/png'));
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <SignatureCanvas ref={sigCanvas} penColor='black'
+        canvasProps={{ minWidth: 200, maxWidth: 500, width: 500, height: 200, className: 'sigCanvas border-1 bg-white' }} />
+      <div className="flex flex-row justify-center items-center">
+        {/* reset button */}
+        <button className="m-4 bg-blue-500 text-white px-4 py-2 rounded" onClick={onClear}>
+          Clear
+        </button>
+        {/* save button */}
+        <button className="bg-green-500 text-white px-4 py-2 rounded" onClick={onSave}>
+          Save
+        </button>
+      </div>
+      <div className="h-8" />
+      <div style={{ width: '500px', height: '200px', position: 'relative' }} className="border-1 bg-white">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src={url}       // your image path
+          alt="Example"
+          fill                     // this makes the image fill the container
+          style={{ objectFit: 'contain' }} // or 'contain' if you want to keep aspect ratio
+          sizes="(max-width: 500px) 100vw, 200px"
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      {/* download button */}
+      <div className="h-8" />
+      <a href={url} download="signature.png" className="bg-yellow-500 text-white px-4 py-2 rounded">
+        Download
+      </a>
+
+      {/* <Image src={url} alt="signature" width={500} height={200} className="border-1 bg-white" /> */}
     </div>
   );
 }
+
